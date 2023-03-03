b6ca81cebbc6d597d59f6a23763640b66a7bada2
michael <jipmichael1@gmail.com>
2023-03-03 11:21:51 +0700

    configpassport

--- server.js
+++ server.js
@@ -35,9 +35,24 @@ app.use(
 );
 
 app.use(cookieParser("secretcode"));
+app.use(passport.initialize());
+app.use(passport.session());
+
+import { passport } from "./passportConfig.js";
+
 //Routes
 app.post("/login", (req, res) => {
-  console.log(req.body);
+  passport.authenticate("local", (err, user, info) => {
+    if (err) throw err;
+    if (!user) res.send("No User Exists");
+    else {
+      req.logIn(user, (err) => {
+        if (err) throw err;
+        res.send("Successfully Authenticated");
+        console.log(req.user);
+      });
+    }
+  })(req, res, next);
 });
 
 app.post("/register", async (req, res) => {
