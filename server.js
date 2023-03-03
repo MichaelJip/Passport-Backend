ffdcaccd89800a0dc5dd4e57ae3514dd96c95664
michael <jipmichael1@gmail.com>
2023-03-03 11:06:00 +0700

    bisa ternyata

--- server.js
+++ server.js
@@ -55,6 +55,11 @@ app.post("/register", async (req, res) => {
     username,
     password,
   });
+
+  return res.send(201).json({
+    success: true,
+    message: "Welcome new user",
+  });
 });
 
 app.get("/get", (req, res) => {});
